6977cb73ed7757d76b11daf6680df9c3ff89bd7c
Rinpaz <ricardo.filho@icomp.ufam.edu.br>
2025-08-08 23:44:34 -0300

    Initial commit: project setup with TypeScript, Jest, pipeline, and example test

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest';
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
+
+
